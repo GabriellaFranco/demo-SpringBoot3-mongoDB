9778222b6db0c4414b3ddb7e4d4c97c7d8e13669
Gabriella Franco <gabriella-nena@hotmail.com>
2024-12-14 18:45:03 -0300

    Exception Handling

--- src/main/java/com/enterprise/services/UserService.java
+++ src/main/java/com/enterprise/services/UserService.java
@@ -1,12 +1,14 @@
 package com.enterprise.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enterprise.domain.User;
 import com.enterprise.repository.UserRepository;
+import com.enterprise.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 
 }
