949d2fe514b0465146d2393e6ed9cf132ca52e14
Gabriella Franco <gabriella-nena@hotmail.com>
2024-12-14 20:38:00 -0300

    Created AuthorDTO

--- src/main/java/com/enterprise/config/Instantiation.java
+++ src/main/java/com/enterprise/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.enterprise.domain.Post;
 import com.enterprise.domain.User;
+import com.enterprise.dto.AuthorDTO;
 import com.enterprise.repository.PostRepository;
 import com.enterprise.repository.UserRepository;
 
@@ -34,8 +35,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
