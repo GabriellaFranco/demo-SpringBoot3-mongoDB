58c1e9d254b4199513c02d4437734e0f7ece3f99
Gabriella Franco <gabriella-nena@hotmail.com>
2024-12-14 20:27:21 -0300

    Created Post entity

--- src/main/java/com/enterprise/config/Instantiation.java
+++ src/main/java/com/enterprise/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.enterprise.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.enterprise.domain.Post;
 import com.enterprise.domain.User;
+import com.enterprise.repository.PostRepository;
 import com.enterprise.repository.UserRepository;
 
 @Configuration
@@ -14,10 +17,15 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		postRepository.deleteAll();
 		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -25,6 +33,12 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo, abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		
 	}
 
---
+++ src/main/java/com/enterprise/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.enterprise.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.enterprise.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>  {
+
+}
