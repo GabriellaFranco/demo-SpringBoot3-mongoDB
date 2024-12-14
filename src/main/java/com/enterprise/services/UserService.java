14fb3f700019bfb9968232af28505845487dfa7a
Gabriella Franco <gabriella-nena@hotmail.com>
2024-12-14 20:05:15 -0300

    Implemented insert, delete and update User methods

--- src/main/java/com/enterprise/services/UserService.java
+++ src/main/java/com/enterprise/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enterprise.domain.User;
+import com.enterprise.dto.UserDTO;
 import com.enterprise.repository.UserRepository;
 import com.enterprise.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,38 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		if (!userRepository.existsById(id)) {
+			throw new ObjectNotFoundException(id);
+		}
+		
+		userRepository.deleteById(id);
+    }
+	
+	public User update(User obj) {
+		
+		User newObj = userRepository.findById(obj.getId()).orElseThrow(() -> new ObjectNotFoundException(obj.getId()));
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 
 }
